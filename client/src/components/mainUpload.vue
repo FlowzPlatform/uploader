7f2068039b8d1f0c6764c62c135bdc2c28e200cc
Ekta Kaur <ektakaurc@officebrain.com>
2018-04-03 14:22:12 +0530

    console removed

--- client/src/components/mainUpload.vue
+++ client/src/components/mainUpload.vue
@@ -2349,7 +2349,6 @@ export default {
         encoding: "UTF-8",
         skipEmptyLines: true,
         chunk: async function(results, streamer) {
-          console.log("results...",results)
             if(results.data.length != 0){
               totalRecords = totalRecords + results.data.length
               streamer.pause()
@@ -2386,7 +2385,6 @@ export default {
         socket.removeListener('err');
 
         socket.on('response', (response) => {
-          console.log("socket response...",response)
           if(response.stdout.ok == 1){
             resolve(response)
           }
@@ -3134,7 +3132,6 @@ export default {
       })
     },
     async showerrmsg (errcols,tab,schema) {
-      console.log("showerrmsg called...")
      var example1 = document.getElementById('example1')
      let row1
      let col1
@@ -3256,7 +3253,6 @@ export default {
      },
     saveData(tab){
       let self = this
-      console.log("saveData called...")
       self.mObj[tab].load = true
       load_flag = true
       var newCSV = _.map(self.mObj[tab].newUploadCSV, function(element) {
@@ -3267,15 +3263,15 @@ export default {
         "activetab" : tab,
         "newCSV": newCSV
       }
-      console.log("Data emitted...",obj)
+
       socket.emit('pdmData', obj, (err, data) => {
-        console.log("Data emiited inside.....")
+
         if (err) {
           console.log("err...",err)
           self.$Notice.error({title: 'Error!', desc: 'Error in saving the data!'})
         }
         if(data){
-          console.log("socket data.....",data)
+
         }
       })
     self.proceedBtn = true
